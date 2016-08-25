cfe8e3582066a04071eb0cc7b99413ee2d078e77
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-08-25 13:14:23 -0700

    ejercicio2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ejercicio2
+# Diagrama de Flujo
+
+1. Inicio
+2. Ingrese num1
+3. Ingrese num2
+4. Calcular suma=num1+num2
+5. Resultado suma
+6. Fin
+
+![image](promedio.jpg)
---
+++ js/app.js
@@ -0,0 +1,5 @@
+var n1 = parseInt(prompt("Inserte numero1"));
+var n2 = parseInt(prompt("Inserte numero2"));
+var n3 = parseInt(prompt("Inserte numero3"));
+promedio=(n1+n2+n3)/3;
+document.write("El promedio es :  " +promedio);
